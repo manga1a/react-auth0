d81c6f7af658ec51e64407a02f3b62bb11f688db
Mangala <m@mangala.dev>
2022-02-15 19:35:41 +1100

    Add logout.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./Home";
 import Profile from "./Profile";
 import Nav from "./Nav";
 import Auth from "./Auth/Auth";
+import Callback from "./Callback";
 
 class App extends Component {
   constructor(props) {
@@ -14,12 +15,16 @@ class App extends Component {
   render() {
     return (
       <>
-        <Nav />
+        <Nav auth={this.auth} />
         <div className="body">
           <Route
             path="/"
             exact
             render={(props) => <Home auth={this.auth} {...props} />}
+          />
+          <Route
+            path="/callback"
+            render={(props) => <Callback auth={this.auth} {...props} />}
           />
           <Route path="/profile" component={Profile} />
         </div>
